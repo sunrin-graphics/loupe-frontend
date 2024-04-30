7baf823968b311fd87704000b59c178ac7ce1a66
jureuk7 <jureuk7@protonmail.com>
2024-04-30 14:14:08 +0900

    Fix: lint error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import styled, { keyframes } from 'styled-components';
 import Logo from '../assets/logo.svg';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
