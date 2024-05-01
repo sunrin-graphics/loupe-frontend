c5452308bbc113e554a846004a6da4cb29f6f6c8
iwopant0825 <129718228+iwopant0825@users.noreply.github.com>
2024-05-01 18:29:03 +0900

    이상한 구체체map

--- src/components/MainBackgroundSphere.tsx
+++ src/components/MainBackgroundSphere.tsx
@@ -1,13 +1,15 @@
-import React, { useRef, useState, Suspense } from 'react';
+import React, { useRef, useState, Suspense, useEffect } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Sphere, useTexture, } from '@react-three/drei';
+import { Sphere, useTexture,OrbitControls } from '@react-three/drei';
 import * as THREE from 'three'
 import styled from 'styled-components';
+import { useSpring, animated } from '@react-spring/three'
 
 export default function MainBackgroundSphere(p) {
     return (
         <MaibLayout>
             <Canvas gl={{ antialias: true }} shadows style={{ width: "100%", height: '100%', zIndex: "99" }}>
+                <OrbitControls/>
                 <SphereMesh/>
             </Canvas>
         </MaibLayout>
@@ -18,12 +20,34 @@ export default function MainBackgroundSphere(p) {
 function SphereMesh(props) {
     const map = useTexture("/test.png")
     const ref = useRef();
+    const [on,setOn]=useState(false)
+
+    const spring1 = useSpring({
+      position : on? [0,-2.5,0]:[0,-4,0],
+      rotation : on? [0,1.5,0]:[0,0,0],
+      config: { duration: 1000,},
+    })
+
+    const spring2 = useSpring({
+      position : on? [0.1,-2.5,0]:[0.1,-4,0],
+      config: { duration: 1000,},
+    })
+
+    useEffect(() => {
+      setOn(true);
+  }, []);
+
     return (
       <>
         <ambientLight intensity={5}/>
-        <Sphere rotation={[0,0,0]} ref={ref} position={[0,-2.5,0]} scale={2.75}>
+        <animated.mesh rotation={spring1.rotation} position={spring2.position} scale={2.9}>
+          <sphereGeometry/>
+          <meshStandardMaterial color={'#26184a'} side={THREE.BackSide} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.5} />
+        </animated.mesh>
+        <animated.mesh rotation={spring1.rotation} position={spring1.position} scale={2.75}>
+          <sphereGeometry/>
           <meshPhysicalMaterial side={THREE.BackSide} map={map} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.5} />
-        </Sphere>
+        </animated.mesh>
       </>
     )
   }
