6a17623c008b1740398798e675f8260f77dde13a
PleBea <bini060315kim@gmail.com>
2024-05-02 00:13:59 +0900

    fix: type error

--- src/components/ThreeBackground.tsx
+++ src/components/ThreeBackground.tsx
@@ -1,25 +1,35 @@
 import { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Points, PointMaterial,Stars} from '@react-three/drei';
-import * as random from 'maath/random/dist/maath-random.esm';
+import { Points, PointMaterial } from '@react-three/drei';
+
+import { random } from 'maath';
+import type { Group } from 'three';
 
 export default function ThreeBackground() {
   return (
-    <Canvas camera={{type:'orthographic'}} style={{ position: 'absolute', zIndex: '999' }}>
-      <Starss />
+    <Canvas
+      camera={{ type: 'orthographic' }}
+      style={{ position: 'absolute', zIndex: '999' }}
+    >
+      <Stars />
     </Canvas>
   );
 }
 
-function Starss(props) {
-  const ref = useRef();
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 5.5 }),
+function Stars() {
+  const ref = useRef<Group>(null);
+  const [sphere] = useState(
+    () =>
+      new Float32Array(
+        random.inSphere(new Float32Array(5000), { radius: 5.5 }),
+      ),
   );
 
   useFrame((state, delta) => {
-    const mouseX = (state.mouse.x * state.viewport.width) / 2;
-    const mouseY = (state.mouse.y * state.viewport.height) / 2;
+    if (!ref.current) return;
+
+    const mouseX = (state.pointer.x * state.viewport.width) / 2;
+    const mouseY = (state.pointer.y * state.viewport.height) / 2;
 
     ref.current.rotation.x = (mouseY - ref.current.position.y) * 0.005;
     ref.current.rotation.y = (mouseX - ref.current.position.x) * 0.005;
@@ -30,10 +40,10 @@ function Starss(props) {
 
   return (
     <group ref={ref} rotation={[0, 0, Math.PI / 4]}>
-      <Points positions={sphere} stride={3} frustumCulled={false} {...props}>
+      <Points positions={sphere} stride={3} frustumCulled={false}>
         <PointMaterial
           transparent
-          opacity={0.5} 
+          opacity={0.5}
           color="#B880FF"
           size={0.02}
           sizeAttenuation={true}
