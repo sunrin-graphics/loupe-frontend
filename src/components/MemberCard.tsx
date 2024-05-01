85fa91f3816ea766de49249838eb6a4dd8a41a26
박찬규 <126973231+pck-llight@users.noreply.github.com>
2024-05-01 18:52:01 +0900

    멤버 카드 분리 (props)

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -4,7 +4,7 @@ import ArtIcon from '../assets/art.svg?react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-const MemberCard = (/*이름, 사진, 링크3개*/) => {
+const MemberCard = ({name, mail, insLink, poLink, artLink}) => {
   const imgAnimation = {
     show: {
       y: [40, 0],
@@ -21,22 +21,22 @@ const MemberCard = (/*이름, 사진, 링크3개*/) => {
     <MemberCardLayout variants={imgAnimation}>
       <MemberCardImg src={'author.png'} alt={'이미지'} />
       <div>
-        <MemberCardName>박시원</MemberCardName>
-        <MemberCardMailaddress>whoisapple@kakao.com</MemberCardMailaddress>
+        <MemberCardName>{name}</MemberCardName>
+        <MemberCardMailaddress>{mail}</MemberCardMailaddress>
       </div>
-      <CardLinkItem>
+      <CardLinkItem href={insLink}>
         <CardLinkIcon>
           <InstagramIcon />
         </CardLinkIcon>
         <CardLinkTitle color={'#59596F'}>인스타그램</CardLinkTitle>
       </CardLinkItem>
-      <CardLinkItem>
+      <CardLinkItem href={poLink}>
         <CardLinkIcon>
           <LinkIcon />
         </CardLinkIcon>
         <CardLinkTitle color={'#BBBBC4'}>포트폴리오</CardLinkTitle>
       </CardLinkItem>
-      <CardLinkItem>
+      <CardLinkItem href={artLink}>
         <CardLinkIcon>
           <ArtIcon />
         </CardLinkIcon>
@@ -47,56 +47,58 @@ const MemberCard = (/*이름, 사진, 링크3개*/) => {
 };
 
 const MemberCardLayout = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
 `;
 
 const MemberCardImg = styled.img`
-  width: 56px;
-  height: 56px;
-  border-radius: 56px;
+    width: 56px;
+    height: 56px;
+    border-radius: 56px;
 `;
 
 const MemberCardName = styled.div`
-  color: var(--100, #181826);
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 150%; /* 27px */
+    color: var(--100, #181826);
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 150%; /* 27px */
 `;
 
 const MemberCardMailaddress = styled.div`
-  color: var(--100, #181826);
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 150%; /* 24px */
+    color: var(--100, #181826);
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150%; /* 24px */
 `;
 
-const CardLinkItem = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
+const CardLinkItem = styled.a`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    text-decoration: none;
 `;
 
 const CardLinkTitle = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 4px;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 150%; /* 21px */
-  color: ${(props) => props.color || 'red'};
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150%; /* 21px */
+    color: ${(props) => props.color || 'red'};
+    text-decoration: none;
 `;
 const CardLinkIcon = styled.div`
-  display: flex;
-  width: 20px;
-  height: 20px;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    width: 20px;
+    height: 20px;
+    justify-content: center;
+    align-items: center;
 `;
 
 export default MemberCard;
