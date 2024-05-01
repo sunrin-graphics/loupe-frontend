d1ae63a4d838334458c47dbc69926033693b7178
jureuk7 <jureuk7@protonmail.com>
2024-05-01 19:10:24 +0900

    Fix: conflict

--- src/components/MainBackgroundSphere.tsx
+++ src/components/MainBackgroundSphere.tsx
@@ -1,18 +1,32 @@
-import { Box, OrbitControls, Sphere } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import styled from "styled-components";
+import React, { useRef, useState, Suspense } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Sphere, useTexture, } from '@react-three/drei';
+import * as THREE from 'three'
+import styled from 'styled-components';
 
-export default function MainBackgroundSphere(p){
-    return(
+export default function MainBackgroundSphere(p) {
+    return (
         <MaibLayout>
-            <Canvas gl={{antialias:true}} shadows style={{width:"100%",height:'100%', zIndex:"999"}}>
-                <OrbitControls enablePan={false} enableZoom={false}/>
-                <Sphere scale={2.75}/>
-            </Canvas>            
+            <Canvas gl={{ antialias: true }} shadows style={{ width: "100%", height: '100%', zIndex: "99" }}>
+                <SphereMesh/>
+            </Canvas>
         </MaibLayout>
 
     )
 }
+
+function SphereMesh(props) {
+    const map = useTexture("/test.png")
+    const ref = useRef();
+    return (
+      <>
+        <ambientLight intensity={5}/>
+        <Sphere rotation={[0,0,0]} ref={ref} position={[0,-2.5,0]} scale={2.75}>
+          <meshPhysicalMaterial side={THREE.BackSide} map={map} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.5} />
+        </Sphere>
+      </>
+    )
+  }
 
 const MaibLayout = styled.div`
     width:100%;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,21 +1,24 @@
 import styled from 'styled-components';
 import ThreeBackground from '../components/ThreeBackground';
 import MainBackground from '../components/MainBackground';
-import HeaderTransparent from '../components/HeaderTransparent';
+import MainBackgroundSphere from '../components/MainBackgroundSphere';
 
 export default function Main() {
   return (
     <MainLayout>
-      <HeaderTransparent />
-      <ThreeBackground />
-      <MainBackground />
+      <MainBackgroundSphere/>
+      <ThreeBackground/>
+      <MainBackground/>
+      <MainBackgroundSphere/>
     </MainLayout>
   );
 }
 
 const MainLayout = styled.div`
-  background-color: #2e2053;
-  position: relative;
-  width: 100%;
-  height: 100vh;
-`;
+  background-color:#2E2053;
+  position:relative;
+  width:100%;
+  height:100vh;
+`
+
+
