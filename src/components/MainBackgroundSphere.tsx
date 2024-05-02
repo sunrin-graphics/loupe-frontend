2442b31d6face6ce5d6036eb85a2e76df2e2f652
PleBea <bini060315kim@gmail.com>
2024-05-02 16:57:12 +0900

    fix sphere type error

--- src/components/MainBackgroundSphere.tsx
+++ src/components/MainBackgroundSphere.tsx
@@ -3,6 +3,7 @@ import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Environment, useTexture } from '@react-three/drei';
 import styled from 'styled-components';
 import { useSpring, animated } from '@react-spring/three';
+import { PointLight } from 'three';
 
 export default function MainBackgroundSphere() {
   return (
@@ -13,29 +14,33 @@ export default function MainBackgroundSphere() {
         shadows
         style={{ width: '100%', height: '100%', zIndex: '99' }}
       >
-        <Light/>
-        <Environment preset='city'/>
+        <Light />
+        <Environment preset="city" />
         <SphereMesh />
       </Canvas>
     </MaibLayout>
   );
 }
 
-function Light(){
-  const lightPosition = [3,0,1]
-  const lightRef = useRef();
-  const { mouse } = useThree();
+function Light() {
+  const lightRef = useRef<PointLight>(null);
+  const { pointer } = useThree();
   useFrame(() => {
     if (lightRef.current) {
-      lightRef.current.position.x = mouse.x * 2;
-      lightRef.current.position.y = mouse.y * 2; 
+      lightRef.current.position.x = pointer.x * 2;
+      lightRef.current.position.y = pointer.y * 2;
     }
   });
-  return(
+  return (
     <>
-      <pointLight ref={lightRef} intensity={60} position={lightPosition} distance={100} />
+      <pointLight
+        ref={lightRef}
+        intensity={60}
+        position={[3, 0, 1]}
+        distance={100}
+      />
     </>
-  )
+  );
 }
 
 function SphereMesh() {
@@ -49,13 +54,16 @@ function SphereMesh() {
   useEffect(() => {
     springApi.start({
       position: [0, -3.7, 0],
-    })
-  })
+    });
+  });
   return (
     <>
-      <animated.mesh scale={3.8} position={spring.position}>
+      <animated.mesh
+        scale={3.8}
+        position={spring.position.to((x, y, z) => [x, y, z])}
+      >
         <circleGeometry />
-        <meshStandardMaterial  metalness={1} map={map} />
+        <meshStandardMaterial metalness={1} map={map} />
       </animated.mesh>
     </>
   );
