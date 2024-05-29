33febce3a9d634b891d5c62365ac78d36f35bc03
PleBea <bini060315kim@gmail.com>
2024-05-29 15:59:20 +0900

    fix: build error

--- src/components/common/OpenRouter.tsx
+++ src/components/common/OpenRouter.tsx
@@ -1,15 +1,21 @@
 import useIsOpenStore from '@/store/isOpen';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
 export default function OpenRouter() {
   const isOpen = useIsOpenStore((state) => state.isOpen);
   const load = useIsOpenStore((state) => state.load);
 
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (load && !isOpen) {
+  //     return <Navigate to="/" />;
+  //   }
+  // }, [load]);
+
   useEffect(() => {
-    if (load && !isOpen) {
-      return <Navigate to="/" />;
-    }
+    if (load && !isOpen) return navigate('/');
   }, [load]);
 
   return <Outlet />;
